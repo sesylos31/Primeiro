64f4dd947055fdf256ee6608b1f4c208fc5d0f56
Sergio Sylos <sergio.sylos@zienic.com.br>
2020-06-10 00:48:07 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+                    }
+
+        public SalesRecord(int id, DateTime darte, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = darte;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
